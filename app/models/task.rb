6a320d1164c52dcc0827bc2c52ca66950ec887ff
Evgeniy Zabolotniy <evgeniy.zabolotniy@gmail.com>
2013-05-07 15:42:54 +0300

    new feature

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,7 +4,9 @@ class Task < ActiveRecord::Base
   validates_presence_of :title
 
   DEFAULT_STRING = "Test data"
-  def test_function
-    "#{title}-#{DEFAULT_STRING}"
+  def test_function value=nil
+    str = "#{title}-#{DEFAULT_STRING}"
+    str += "+#{value}" if value.present?
+    str
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -7,5 +7,10 @@ describe Task do
       t = Task.create title: 'Trol'
       t.test_function.should == "#{t.title}-#{Task::DEFAULT_STRING}"
     end
+
+    it "return value with arg" do
+      t = Task.create title: 'Trol'
+      t.test_function('Olololo').should == "#{t.title}-#{Task::DEFAULT_STRING}+Olololo"
+    end
   end
 end
